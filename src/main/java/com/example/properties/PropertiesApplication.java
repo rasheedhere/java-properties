4eab2dc4b88f889c04d7137a695e1abe8bedf92c
Rasheedulla Khan <rasheedhere@gmail.com>
2019-04-14 14:42:21 -0400

    Added database configuration

--- src/main/java/com/example/properties/PropertiesApplication.java
+++ src/main/java/com/example/properties/PropertiesApplication.java
@@ -1,10 +1,14 @@
 package com.example.properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PropertiesApplication {
+
+	@Autowired
+	DatabaseConfiguration databaseConfiguration;
 
 	public static void main(String[] args) {
 		SpringApplication.run(PropertiesApplication.class, args);
--- src/test/java/com/example/properties/PropertiesApplicationTests.java
+++ src/test/java/com/example/properties/PropertiesApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.properties;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -13,8 +14,18 @@ public class PropertiesApplicationTests {
 	@Value("${url}")
 	String url;
 
+	@Autowired
+	DatabaseConfiguration databaseConfiguration;
+
 	@Test
 	public void contextLoads() {
 		Assert.assertEquals(url, "www.google.com");
+	}
+
+	@Test
+	public void dataConfigurationTests() {
+		Assert.assertEquals(databaseConfiguration.getUrl(), "172.16.21.5");
+		Assert.assertEquals(databaseConfiguration.getUsername(), "admin");
+		Assert.assertEquals(databaseConfiguration.getPassword(), "admin");
 	}
 }
